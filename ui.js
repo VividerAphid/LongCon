6e1a0543056727636ffe8eb08c7edd4d247bc4ef
VividerAphid <trever.kress@gmail.com>
2025-03-23 21:33:49 -0400

    Added validateElementMinMax() for all inputs

--- ui.js
+++ ui.js
@@ -6,6 +6,15 @@ function addElement(id, type, parent, innards){
     return child;
 }
 
+function validateElementMinMax(element){
+    if((element.value * 1) > (element.max * 1)){
+        element.value = element.max;
+    }
+    if((element.value * 1) < (element.min * 1)){
+        element.value = element.min;
+    }
+}
+
 function toggleFactionCountSelect(){
     if(factionCountdrp.selectedIndex == 0){
         factionCountinp.style.display = "none";
@@ -33,12 +42,7 @@ function toggleCoordinatorChxDisabled(){
 }
 
 function factionCountChange(){
-    if((factionCountinp.value * 1) > (factionCountinp.max * 1)){
-        factionCountinp.value = factionCountinp.max;
-    }
-    if((factionCountinp.value * 1) < (factionCountinp.min * 1)){
-        factionCountinp.value = factionCountinp.min;
-    }
+    validateElementMinMax(factionCountinp);
     playerCountinp.max = Math.round(250 / factionCountinp.value);
     if((playerCountinp.value * 1) > (playerCountinp.max * 1)){
         playerCountinp.value = playerCountinp.max;
@@ -48,12 +52,7 @@ function factionCountChange(){
 }
 
 function playerCountChange(){
-    if((playerCountinp.value * 1) > (playerCountinp.max * 1)){
-        playerCountinp.value = playerCountinp.max;
-    }
-    if((playerCountinp.value * 1) < (playerCountinp.min * 1)){
-        playerCountinp.value = playerCountinp.min;
-    }
+    validateElementMinMax(playerCountinp);
 }
 
 function initFactionListings(){
@@ -75,21 +74,21 @@ function initFactionListings(){
         let rDiv = addElement("rDiv"+r, "div", rgbDiv, "R: ");
         let rInp = addElement("rInp"+r, "input", rDiv);
         rInp.value = colors[2][0];
-        rInp.onchange = function(){colors[2][0] = rInp.value; updateFactionColor(r, colors); initFactionListings();};
+        rInp.onchange = function(){validateElementMinMax(rInp); colors[2][0] = rInp.value; updateFactionColor(r, colors); initFactionListings();};
         rInp.size = inpSize;
         rInp.min = 0;
         rInp.max = 255;
         let gDiv = addElement("gDiv"+r, "div", rgbDiv, "G: ");
         let gInp = addElement("gInp"+r, "input", gDiv);
         gInp.value = colors[2][1];
-        gInp.onchange = function(){colors[2][1] = gInp.value; updateFactionColor(r, colors); initFactionListings();};
+        gInp.onchange = function(){validateElementMinMax(gInp); colors[2][1] = gInp.value; updateFactionColor(r, colors); initFactionListings();};
         gInp.size = inpSize;
         gInp.min = 0;
         gInp.max = 255;
         let bDiv = addElement("bDiv"+r, "div", rgbDiv, "B: ");
         let bInp = addElement("bInp"+r, "input", bDiv);
         bInp.value = colors[2][2];
-        bInp.onchange = function(){colors[2][2] = bInp.value; updateFactionColor(r, colors); initFactionListings();};
+        bInp.onchange = function(){validateElementMinMax(bInp); colors[2][2] = bInp.value; updateFactionColor(r, colors); initFactionListings();};
         bInp.size = inpSize;
         bInp.min = 0;
         bInp.max = 255;
